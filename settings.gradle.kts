736a9ce9a60c8c6ea2ab6a51fc2f951689ea826e
Russell <russhwolf@gmail.com>
2020-11-27 20:57:14 -0500

    Core module builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.library" -> useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+}
+
+include(":internal-testutils-truth")
 include(":datastore-core")
